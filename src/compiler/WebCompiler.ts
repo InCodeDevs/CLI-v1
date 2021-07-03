86d2d0d60f180322697eb25b3ddf3e1a76b65b77
MCTzOCK <dev@mctzock.de>
2021-07-03 20:01:06 +0200

    Changed some stuff and added other stuff

--- src/compiler/WebCompiler.ts
+++ src/compiler/WebCompiler.ts
@@ -53,22 +53,122 @@ export class WebCompiler {
     }
 
     static compile(code: string): string {
-        console.log(code)
         // prepare the code
         // trim the code
         code = code.trim()
         // remove comments
-        code = code.replace(/\/\/(.|\s)*\/\//, "");
+
+        // code = code.replace(/\/\/(.|\s)*\/\//, "");
+
         // parse the code
         // create var for result
         let result: string = ""
+
+        // split by line breaks
+        let expressions = code.split("\n");
+
+        // create an empty array for all code blocks
+        let codeBlocks = []
+
+        // create an empty array for ignored lines
+        let ignoredLines = []
+
+        // loop through all expressions
+        for (let i = 0; i < Object.keys(expressions).length; i++) {
+
+            // get the current expression
+            let expression = expressions[i]
+
+            if (!expression.startsWith("//")) {
+
+                // check if the expression is not \r
+                if (expression != "\r" && !ignoredLines.includes(i)) {
+
+                    // get the position of this expression
+                    let rootCodeBlockPosition = this.getCodeBlockPosition(expression)
+
+                    if (rootCodeBlockPosition === 0) {
+
+                        let exs: string[] = this.getEXS(rootCodeBlockPosition, expressions, i)
+
+                        if (exs.length == 0) {
+                            codeBlocks.push({
+                                line: i,
+                                position: rootCodeBlockPosition,
+                                statement: expression,
+                                innerStatements: []
+                            })
+                        } else {
+
+                            let block = {
+                                line: i,
+                                position: rootCodeBlockPosition,
+                                statement: expression,
+                                innerStatements: []
+                            };
+
+                            for (let j = 0; j < exs.length; j++) {
+                                if (this.getCodeBlockPosition(exs[j]) > rootCodeBlockPosition) {
+                                    if (this.getCodeBlockPosition(exs[j]) - 1 === rootCodeBlockPosition) {
+                                        block.innerStatements.push({
+                                            line: i,
+                                            position: this.getCodeBlockPosition(exs[j]),
+                                            statement: exs[j],
+                                            innerStatements: []
+                                        })
+                                    } else {
+                                        if (block.innerStatements.length > 0) {
+                                            console.log(block.innerStatements.length)
+                                            block.innerStatements[block.innerStatements.length - 1].innerStatements.push({
+                                                line: i,
+                                                position: this.getCodeBlockPosition(exs[j]),
+                                                statement: exs[j],
+                                                innerStatements: []
+                                            })
+                                        }
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
+                            codeBlocks.push(block)
+                        }
+                    }
+                }
+            }
+        }
+
+        // compile each codeBlock
+        codeBlocks.forEach(codeBlock => {
+            console.log(JSON.stringify(codeBlock))
+        })
 
         // return the final javascript code
         return result
     }
 
-    static compileStatement(statement: string): string {
+    public static getEXS(rootCodeBlockPosition: number, expressions: string[], i: number): string[] {
+        let exs: string[] = []
+        for (let j = i + 1; j < expressions.length; j++) {
+            if (expressions[j]) {
+                if (this.getCodeBlockPosition(expressions[j]) > rootCodeBlockPosition) {
+                    exs.push(expressions[j])
+                } else {
+                    break
+                }
+            } else {
+                break
+            }
+        }
+        return exs
+    }
+
+    public static compileStatement(statement: string): string {
         return ""
     }
 
+    protected static getCodeBlockPosition(expression: string): number {
+        const re = /\t/g
+        return ((expression || '').match(re) || []).length
+    }
 }
