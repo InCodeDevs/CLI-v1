6cc7aed3795fd8c8e374371b3ea6cfdef5dcfa28
Ben Siebert <dev@mctzock.de>
2021-09-03 21:38:58 +0200

    Added STD Output Option

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ const { disableConsole, enableConsole } = require('./util');
 program.version("2.0.0");
 
 program
-    .requiredOption("-i, --input <file>", "Specifies the File that shoudl be compiled")
+    .requiredOption("-i, --input <file>", "Specifies the File that should be compiled")
     .option("-o, --output <directory>", "Specifies the output directory of the application", "dist")
     .option("-f, --force", "Force overwrite existing files")
+    .option('-u, --use-std', "Uses stdout as output")
 
 program.parse(process.argv);
 
@@ -28,6 +29,14 @@ if(!fs.existsSync(input)){
     return;
 }
 
+const inCode = fs.readFileSync(input).toString();
+
+disableConsole();
+
+const jsCode = WebCompiler.compile(inCode);
+
+enableConsole();
+
 let shouldReCreateDistFolder = true;
 
 if(fs.existsSync(output)){
@@ -41,18 +50,15 @@ if(fs.existsSync(output)){
     }
 }
 
-const inCode = fs.readFileSync(input).toString();
+if(!options.useStd){
+    let html = '<script defer src="' + input  + '.js"></script>';
 
-disableConsole();
+    if(shouldReCreateDistFolder)
+        fs.mkdirSync(output);
 
-const jsCode = WebCompiler.compile(inCode);
-
-enableConsole();
-
-let html = '<script defer src="' + input  + '.js"></script>';
-
-if(shouldReCreateDistFolder)
-    fs.mkdirSync(output);
-
-fs.writeFileSync(path.join(output, "/index.html"), html);
-fs.writeFileSync(path.join(output, input + ".js"), jsCode)
+    fs.writeFileSync(path.join(output, "/index.html"), html);
+    fs.writeFileSync(path.join(output, input + ".js"), jsCode)
+}else {
+    let html = '<script defer>' + jsCode + '</script>'
+    console.log(html);
+}
